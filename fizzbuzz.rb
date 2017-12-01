109f86695f29acc6f73e94157efe07f2bdbb451d
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-01 04:25:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,4 +7,13 @@ def fizzbuzz(int)
   
   a = div_three.is_a?(Integer)
   b = div_five.is_a?(Integer)
-  if 
+  if a&&b
+    puts "FizzBuzz"
+  elseif a && !b
+    puts "Buzz"
+  elseif b && !a
+    puts "Fizz"
+  else
+    puts "nil"
+  end
+end
