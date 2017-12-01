83c0842c7e702c90f69b42ce146655f1b677101e
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-01 04:47:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,14 @@ def fizzbuzz(int)
   
   int = int.to_i
   
-  div_three = int/3
-  div_five = int/5
-  
   a = int % 3 == 0
   b = int % 5 == 0
   if a&&b
     return "FizzBuzz"
   elseif a && !b
-    return "Buzz"
-  elseif b && !a
     return "Fizz"
+  elseif b && !a
+    return "Buzz"
   else
     return nil
   end
